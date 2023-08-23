31a735399ab58ff009e835b5c3594e1d6ebe3145
Asher McConnell <asherryan7@gmail.com>
2023-08-23 16:46:29 -0600

    Initial Commit

---
+++ CSGO-InventorySystem/CSGO_InventorySystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSGO_InventorySystemApp.swift
+//  CSGO-InventorySystem
+//
+//  Created by Asher McConnell on 8/23/23.
+//
+
+import SwiftUI
+
+@main
+struct CSGO_InventorySystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CSGO-InventorySystem/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CSGO-InventorySystem
+//
+//  Created by Asher McConnell on 8/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
